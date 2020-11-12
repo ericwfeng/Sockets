b86102ebb946c1a50ff785fb120163c5f92cc7f5
Eric W. Feng <65430860+ericwfeng@users.noreply.github.com>
2020-11-11 21:55:08 -0500

    Add files via upload

---
+++ MockGS.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 19:57:25 2020
+
+@author: ericw
+"""
+
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    #s.sendall(b'Hello World')
+    while True:
+       # command = input()
+        #s.send(command.encode('utf-8'))
+    #s.send(command.encode('utf-8'))
+        data = s.recv(1024)
+        if not data:
+            break
+        print(data)
