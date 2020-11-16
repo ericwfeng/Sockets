e2857cb752db4dc6cf673198d5546fbc0a8b5143
Eric W. Feng <65430860+ericwfeng@users.noreply.github.com>
2020-11-16 11:55:03 -0500

    Add files via upload

---
+++ flag.py
@@ -0,0 +1,6 @@
+#flag.py: initializes the actions that can be taken
+class flag:
+   def __init__(self):
+      self.giveWarning = False
+      self.abortFlight = False
+      self.actuatePressureRelease = False
---
+++ registry.py
@@ -0,0 +1,21 @@
+#regstry.py: is able to show the state of the rocket at all times
+class registry:
+   
+   def __init__(self, actuatePressureRelease, pressureSensorReading):
+      self.actuatePressureRelease = actuatePressureRelease
+      self.pressureSensorReading = pressureSensorReading  
+         
+   def viewReadings(self):
+      print("Valve State: " + self.actuatePressureRelease)
+      if self.pressureSensorReading > 460  or self.pressureSensorReading < 50:
+         print("The sensor reading is " + str(self.pressureSensorReading) + " Readings are NORMINAL")
+      else:
+         print("The sensor reading is " + str(self.pressureSensorReading) + " and it is in a critical state.")
+
+      
+      
+      
+      
+      
+      
+        
