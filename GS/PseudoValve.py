36ca1f4039a6843d7f36d22459f7bc04776df492
Eric W. Feng <65430860+ericwfeng@users.noreply.github.com>
2020-11-16 13:42:43 -0500

    Delete PseudoValve.py

--- GS/PseudoValve.py
+++
@@ -1,9 +0,0 @@
-class PseudoValve:
-    def __init__(self):
-        self.state = 'off'
-    
-    def flip(self):
-        if self.state == 'off':
-            self.state = 'on'
-        else:
-            self.state = 'off'
