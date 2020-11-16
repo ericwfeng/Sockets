1049424060c88b67ea142f3b7936de76a3ff08c5
ani131 <69364231+ani131@users.noreply.github.com>
2020-11-15 20:36:10 -0500

    Add files via upload

---
+++ flag.py
@@ -0,0 +1,8 @@
+#flag.py: initializes the actions that can be taken
+class flag:
+   def __init__(self):
+      self.giveWarning = False
+      self.abortFlight = False
+      self.actuatePressureRelease = False
+      
+flag = flag()
